138c4e44bffd981143816187a22c0638da248789
Alex Tagaev <alex.tagaev@gmail.com>
2024-12-24 21:24:03 +0300

    first commit

---
+++ api/main.py
@@ -0,0 +1,31 @@
+# импорт библиотек
+from fastapi import FastAPI     # библиотека FastAPI
+from pydantic import BaseModel  # модуль для объявления структуры данных
+from api.chunks import Chunk    # модуль для работы с OpenAI
+
+# создаем объект приложения FastAPI
+app = FastAPI()
+
+# создадим объект для работы с OpenAI
+chunk = Chunk()
+
+# класс с типами данных для метода api/get_answer
+class ModelAnswer(BaseModel):
+    text: str    
+
+# функция, которая будет обрабатывать запрос по пути "/"
+# полный путь запроса http://127.0.0.1:8000/
+@app.get("/")
+def root(): 
+    return {"message": "Hello FastAPI"}
+
+# функция, которая обрабатывает запрос по пути "/about"
+@app.get("/about")
+def about():
+    return {"message": "Страница API нейро-консультанта"}
+
+# функция обработки post запроса + декоратор 
+@app.post('/api/get_answer')
+def get_answer(question: ModelAnswer):
+    answer = chunk.get_answer(query = question.text)
+    return {'Answer': answer[0], 'Total requests': answer[1]}    
